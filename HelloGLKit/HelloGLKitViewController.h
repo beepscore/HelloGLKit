82036a771292b7c6cfb1764dd90d0e20d4316ef2
beepscore <support@beepscore.com>
2012-04-25 22:11:22 -0700

    Add class HelloGLKitViewController.

---
+++ HelloGLKit/HelloGLKitViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloGLKitViewController.h
+//  HelloGLKit
+//
+//  Created by Steve Baker on 4/25/12.
+//  Copyright (c) 2012 Beepscore LLC. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface HelloGLKitViewController : GLKViewController
+
+@end
